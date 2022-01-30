6941e00f9b1cf832f943c04a278c6cc28ded4778
ankitkumar098 <87219586+ankitkumar098@users.noreply.github.com>
2022-01-30 21:20:51 +0530

    Add files via upload

---
+++ CODE FILES/script.js
@@ -0,0 +1,6 @@
+var x = document.getElementById("searchsummit");
+x.addEventListener('click', showMsg);
+
+function showMsg(){
+    alert("We are sorry for the issue! Search is currently unable.");
+}
